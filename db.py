59a3d8b674fe26e700ea826be61fbe8b6e01ebd7
ohmyolaaa <ohmyolaaa@gmail.com>
2026-06-05 11:11:18 +0800

    Update new shop design

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_SERVICE_ROLE_KEY"]
+SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 
 CATEGORIES_TABLE = "cay_shop_categories"
 PRODUCTS_TABLE = "cay_shop_products"
